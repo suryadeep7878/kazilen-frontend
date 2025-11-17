9905f9d93fccc55b899d1c09ea0a563be9ede92c
Gov-10 <govindsys1008@gmail.com>
2025-11-17 19:06:53 +0530

    Revert "added more files on Mon Nov 17 07:03:13 PM IST 2025"

--- frontapp/.next/static/chunks/src_app_page_88588bc9.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_90fd6cb8._.js",
-  "static/chunks/src_app_aad4fac0._.js"
-],
-    source: "dynamic"
-});
