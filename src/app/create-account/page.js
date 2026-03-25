4d056de63c4ce2f53aff35a23f1a0b5566264dbd
Rom <romitrajbind0@gmail.com>
2026-03-25 22:43:44 +0530

    !Automated message! Did work at Wed Mar 25 10:43:44 PM IST 2026

--- src/app/create-account/page.js
+++ src/app/create-account/page.js
@@ -1,3 +1,4 @@
+"use client";
 import { Suspense } from "react";
 import CreateAccountClient from "./CreateAccountClient";
 import { useSearchParams } from "next/navigation";
@@ -5,11 +6,11 @@ import { useSearchParams } from "next/navigation";
 export const dynamic = "force-dynamic";
 
 export default function Page() {
-	const param = useSearchParams()
-	const phone = param.get("phone")
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <CreateAccountClient phoneFromQuery={phone}/>
-    </Suspense>
-  );
+	const param = useSearchParams();
+	const phone = param.get("phone");
+	return (
+		<Suspense fallback={<div>Loading...</div>}>
+			<CreateAccountClient phoneFromQuery={phone} />
+		</Suspense>
+	);
 }
