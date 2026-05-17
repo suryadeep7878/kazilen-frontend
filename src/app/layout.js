28505d071b43c9369ca6d9567a5650fc7c4310a8
Rom <romitrajbind0@gmail.com>
2026-05-17 19:43:54 +0530

    !Automated message! Did work at Sun May 17 07:43:54 PM IST 2026

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,39 +1,36 @@
 import "./globals.css";
 import ConditionalHeader from "./components/ConditionalHeader";
-    import ConditionalBottomNav from "./components/ConditionalBottomNav";
+import ConditionalBottomNav from "./components/ConditionalBottomNav";
 import ServiceWorkerRegister from "./ServiceWorkerRegister";
-import LocationLoader from "./components/LocationLoader"
+import LocationLoader from "./components/LocationLoader";
 import Providers from "./providers";
 import NetworkStatus from "./components/NetworkStatus";
-import { NuqsAdapter } from 'nuqs/adapters/next/app';
-import AuthInterceptor from "./components/AuthInterceptor";
+import { NuqsAdapter } from "nuqs/adapters/next/app";
+import AuthGuard from "./components/AuthGuard";
 import BackgroundPoller from "./components/poller";
 
-
 export const metadata = {
-  title: "Kazilen",
-  description: "Rom writing ya disc",
-  manifest: "/manifest.json",
-
+	title: "Kazilen",
+	description: "Rom writing ya disc",
+	manifest: "/manifest.json",
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>
-			<BackgroundPoller />
-        <NuqsAdapter>
-          <Providers>
-            <NetworkStatus />
-            <ServiceWorkerRegister />
-            <ConditionalHeader />
-            <LocationLoader />
-            <AuthInterceptor />
-            {children}
-            <ConditionalBottomNav />
-          </Providers>
-        </NuqsAdapter>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body>
+				<NuqsAdapter>
+					<Providers>
+						<NetworkStatus />
+						<ServiceWorkerRegister />
+						<ConditionalHeader />
+						<LocationLoader />
+						<BackgroundPoller />
+						<AuthGuard>{children}</AuthGuard>
+						<ConditionalBottomNav />
+					</Providers>
+				</NuqsAdapter>
+			</body>
+		</html>
+	);
 }
